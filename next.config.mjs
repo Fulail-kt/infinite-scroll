4e2cccda75a47129617630c19c2ffe69a08b1780
Fulal-kt <muhamedfulail77@gmail.com>
2024-03-21 06:18:25 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  // Access environment variables from process.env
+  env: {
+    apiKey: process.env.API_KEY,
+    baseUrl: process.env.BASE_URL
+  }
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,85 @@
-import Image from "next/image";
+
+'use client'
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { PuffLoader } from "react-spinners";
+
 
 export default function Home() {
+
+  const [videos, setVideos] = useState<any>([]);
+  const [loading, setLoading] = useState(false);
+  const [nextPageToken, setNextPageToken] = useState('');
+  const [searchKey, setSearchKey] = useState('');
+  const [scrollingStarted, setScrollingStarted] = useState(false);
+
+  const api = process.env.NEXT_PUBLIC_API_KEY
+
+  useEffect(() => {
+    if (searchKey) {
+
+      fetchVideos(searchKey);
+    }
+    fetchVideos('')
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [nextPageToken]);
+
+  const fetchVideos = async (searchKey?: string, nextPageToken: string = '') => {
+    setLoading(true);
+    const response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchKey}&part=snippet&key=${api}&pageToken=${nextPageToken}`);
+    setVideos((prevVideos: any) => [...prevVideos, ...response.data.items]);
+    setNextPageToken(response.data.nextPageToken);
+    setLoading(false);
+  };
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    
+    if (scrollTop + clientHeight >= scrollHeight - 10 && !loading) {
+      setLoading(true);
+      fetchVideos(searchKey, nextPageToken);
+    }
+   
+  };
+
+  const handleSearch = () => {
+    setVideos([]);
+    setNextPageToken('');
+    fetchVideos(searchKey);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex w-full min-h-screen justify-center">
+       <div className="fixed w-full h-44 bg-blue-500 flex items-center justify-center">
+        <h1 className="text-white text-2xl">Welcome to Test Infinite Scroll</h1>
+      </div>
+
+      <div className="fixed h-20 w-full   justify-center flex items-center "> <div className="w-72 h-8 gap-x-10 rounded-md"><input
+        type="text"
+        className="w-2/3 pl-2 h-full rounded-md text-black"
+        placeholder="Search here"
+        value={searchKey}
+        onChange={(e) => setSearchKey(e.target.value)}
+      />
+        <button className="bg-green-700 p-1 ml-5 rounded-md" onClick={handleSearch}>Search</button></div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-full flex flex-col justify-center items-center ">
+        <div className="pt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 ">
+          {videos.map((video: any, index: any) => (
+            <div className="w-72  h-60 rounded-md my-2 border" key={video.id.videoId + `${index}-${Date.now()}`}>
+              <div className="w-72 p-2 h-40 border flex items-center justify-center rounded-md"><img className=" p-1 border w-full h-full" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} /></div>
+              <div className="p-2 h-44"> <h3>{video.snippet.title}</h3></div>
+            </div>
+          ))}
+        </div>
+          {loading && (
+            <div className="w-full flex justify-center items-center">
+                <PuffLoader color="#fff" size={40} />
+            </div>
+          )}
       </div>
     </main>
   );
