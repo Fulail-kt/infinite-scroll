5fa65402810cef17da579d781b490449548ccdce
Fulal-kt <muhamedfulail77@gmail.com>
2024-03-22 11:08:25 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,11 +52,8 @@ export default function Home() {
 
   return (
     <main className="flex w-full min-h-screen justify-center">
-       <div className="fixed w-full h-44 bg-blue-500 flex items-center justify-center">
-        <h1 className="text-white text-2xl">Welcome to Test Infinite Scroll</h1>
-      </div>
 
-      <div className="fixed h-20 w-full   justify-center flex items-center "> <div className="w-72 h-8 gap-x-10 rounded-md"><input
+      <div className="fixed bg-black h-20 w-full   justify-center flex items-center "> <div className="w-72 h-8 gap-x-10 rounded-md"><input
         type="text"
         className="w-2/3 pl-2 h-full rounded-md text-black"
         placeholder="Search here"
@@ -70,7 +67,7 @@ export default function Home() {
         <div className="pt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 ">
           {videos.map((video: any, index: any) => (
             <div className="w-72  h-60 rounded-md my-2 border" key={video.id.videoId + `${index}-${Date.now()}`}>
-              <div className="w-72 p-2 h-40 border flex items-center justify-center rounded-md"><img className=" p-1 border w-full h-full" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} /></div>
+              <div className="w-72 p-2 h-40  flex items-center justify-center rounded-md"><img className=" p-1  w-full h-full" src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} /></div>
               <div className="p-2 h-44"> <h3>{video.snippet.title}</h3></div>
             </div>
           ))}
